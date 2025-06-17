99686598575a06909a5c56f8392396ee41a70592
MatheusTorres2705 <matheus29torres@gmail.com>
2025-06-17 19:55:41 -0300

    first commit

---
+++ lib/models/chassiMes.dart
@@ -0,0 +1,11 @@
+class ChassiItem {
+  final String chassi;
+  final String cliente;
+  final String status;
+
+  ChassiItem({
+    required this.chassi,
+    required this.cliente,
+    required this.status,
+  });
+}
---
+++ lib/services/api.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class CustomHttp {
+  static final Dio client = Dio();
+}
+
+    // fetch(`http://localhost:3000/api/itens-pedido?nunota=${nunota}`),
+        // fetch(`http://localhost:3000/api/cabecalho-pedido?nunota=${nunota}`)
+
+
+        
